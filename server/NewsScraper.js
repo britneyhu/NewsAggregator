440e5c8e148dc740565b2cb2cc1456dfab72ed3c
britneyhu <britneyhu2@gmail.com>
2025-04-13 17:31:50 -0700

    updated logic to delete old articles and update with new articles

--- server/NewsScraper.js
+++ server/NewsScraper.js
@@ -4,9 +4,8 @@ const newsapi = new NewsAPI('80a19c30b708487baab04dcaa502d3e4');
 const {MongoClient, ServerApiVersion} = require('mongodb');
 const url = "mongodb+srv://britneyhu:88888888@newsaggregator.9dqaloe.mongodb.net/?retryWrites=true&w=majority&appName=NewsAggregator";
 
+//retrieves articles from newsapi
 async function getArticles(){
-  // To query top headlines
-// All options passed to topHeadlines are optional, but you need to include at least one of them
   const articles = await newsapi.v2.topHeadlines({
     sources: 'bbc-news, nbc-news, cnn, google-news, abc-news',
     language: 'en',
@@ -16,13 +15,7 @@ async function getArticles(){
   return articles.articles;
 } 
 
-// async function main() {
-//   const articles = await getArticles();
-//   console.log(articles);
-// }
-
-// main();
-
+//creates mongoAtlas client
 const client = new MongoClient(url, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -31,12 +24,15 @@ const client = new MongoClient(url, {
   }
 });
 
+//delete previous articles updates database with new articles
 async function updateMongo(){
   try{
     await client.connect();
 
     const db = client.db("News");
     const col = db.collection("Articles");
+
+    await col.deleteMany({});
 
     const articles = await getArticles();
     await col.insertMany(articles);
