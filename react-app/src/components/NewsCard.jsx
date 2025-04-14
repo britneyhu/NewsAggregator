ce568c7b4d5c1995e44d7a5002271d8d5522d2d4
britneyhu <britneyhu2@gmail.com>
2025-04-13 18:46:43 -0700

    added newscard component and testarticles

--- react-app/src/components/NewsCard.jsx
+++ react-app/src/components/NewsCard.jsx
@@ -1,43 +1,18 @@
-function NewsCard(){
+function NewsCard({articles}){
     return (
-        <div class="row row-cols-1 row-cols-md-2 g-4">
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <div className="row row-cols-1 row-cols-md-2 g-4">
+            {articles.map((article) =>(
+                <div className="col">
+                    <div className="card h-100">
+                    <img src={article.urlToImage} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{article.title}</h5>
+                        <p className="card-text">{article.description}</p>
+                    </div>
+                    </div>
                 </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content.</p>
-                </div>
-                </div>
-            </div>
-            <div class="col">
-                <div class="card">
-                <img src="..." class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                </div>
-                </div>
-            </div>
-            </div>
+            ))}  
+        </div>
     );
 }
 
