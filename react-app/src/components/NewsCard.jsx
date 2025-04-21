0973d875540ed806d644a0b593bdcf61306fafca
britneyhu <britneyhu2@gmail.com>
2025-04-20 17:32:29 -0700

    changed style of news cards

--- react-app/src/components/NewsCard.jsx
+++ react-app/src/components/NewsCard.jsx
@@ -17,13 +17,13 @@ function NewsCard({articles}){
                 <p>No Articles to Display</p>
             ) : (articles.map((article, index) =>(
                 <div key={index} className="col">
-                    <div className="card h-100">
+                    <div className="card h-100 news-card">
                         <img src={article.urlToImage || "/placeholderImage.png"} className="card-img-top" onError={(e)=>{e.target.onerror=null; e.target.src="/placeholderImage.png"}}/>
-                        <div className="card-body">
+                        <div className="card-body news-card-body">
                             <h5 className="card-title">{article.title}</h5>
                             <p className="card-text">{article.description}</p>
                         </div>
-                        <div className="card-footer d-flex align-items-center justify-content-center gap-5">
+                        <div className="card-footer d-flex align-items-center justify-content-center gap-5 news-card-footer">
                             <a href={article.url} style={{fontWeight: "bold" }} className="card-link">{article.source.name}</a>
                             <div className="d-inline">{formatDate(article.publishedAt)}</div>
                             <a href={article.url} className="card-link">Read More</a>
