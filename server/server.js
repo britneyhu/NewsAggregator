4ed628eb34d0f841b45e03aa911b116cb61f806e
britneyhu <britneyhu2@gmail.com>
2025-04-23 17:45:15 -0700

    adding port for deployment

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const searchResult = require("./routes/searchResult");
 const sortAndFilter = require("./routes/sortAndFilter");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use(cors());
