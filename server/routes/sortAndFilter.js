30eabc43ab24f7b120120afcc49ed3c352d37ad1
britneyhu <britneyhu2@gmail.com>
2025-04-24 18:52:38 -0700

    fixed sort and filter to be specific to the current collection

--- server/routes/sortAndFilter.js
+++ server/routes/sortAndFilter.js
@@ -19,15 +19,15 @@ const sortOptionName = (sortOptionStr) =>{
 
 //Takes sortOption and filters sent from frontend and uses them to query mongo and returns queried articles
 router.post("/", async (req, res) => {
-    let {sortOption, filters} = req.body;
+    let {collection, sortOption, filters} = req.body;
 
-    console.log(`From: Server, Received sort/filter request from react (sortOption=${sortOption}, filters=${filters})`);
+    console.log(`From: Server, Received sort/filter request from react (collection=${collection}, sortOption=${sortOption}, filters=${filters})`);
 
     sortOption = sortOptionName(sortOption);
     
     if(filters) filters = Object.entries(filters).filter(([key,value])=>value).map(([key])=>key);
 
-    const results = await mongo.queryMongo("Articles", sortOption, filters);
+    const results = await mongo.queryMongo(collection, sortOption, filters);
 
     console.log(`From: Server, Sort/filter request completed (sortedArticles=${results.length}`);
 
