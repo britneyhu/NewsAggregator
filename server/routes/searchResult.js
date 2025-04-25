7251ff170f938253655003bfc22d0f4aae17724a
britneyhu <britneyhu2@gmail.com>
2025-04-24 19:00:20 -0700

    removed collection field for search result because it is unnecessary

--- server/routes/searchResult.js
+++ server/routes/searchResult.js
@@ -18,15 +18,15 @@ const sortOptionName = (sortOptionStr) =>{
 }
 
 router.post("/", async (req, res) => {
-    let {collection, keyword, sortOption, filters} = req.body;
-    console.log(`From: Server, Received search request from react (collection=${collection}, keyword=${keyword} sortOption=${sortOption}, filters=${filters})`)
+    let {keyword, sortOption, filters} = req.body;
+    console.log(`From: Server, Received search request from react (keyword=${keyword} sortOption=${sortOption}, filters=${filters})`)
 
     sortOption = sortOptionName(sortOption);
 
     if(filters) filters = Object.entries(filters).filter(([key,value])=>value).map(([key])=>key);
 
     await newsApi.getFromKeyword(keyword);
-    results = await mongo.queryMongo(collection, sortOption, filters);
+    results = await mongo.queryMongo("Articles", sortOption, filters);
     console.log(`From: Server, Search request completed (searchedArticles=${results.length})`);
 
     res.json(results);
